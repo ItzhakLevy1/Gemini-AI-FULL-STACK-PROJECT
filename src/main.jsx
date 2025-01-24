27c0a7c1f547c889c04a61ff35b065b10f4e5ba0
Itzhak Levy <itzhaklevydev@gmail.com>
2025-01-24 17:33:21 +0200

    Add routes, logo and basic styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Homepage from './routes/homepage/Homepage';
-import DashboardPage from './routes/dashboardPage/DashboardPage';
-import ChatPage from './routes/chatPage/ChatPage';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Homepage from "./routes/homepage/Homepage";
+import DashboardPage from "./routes/dashboardPage/DashboardPage";
+import ChatPage from "./routes/chatPage/ChatPage";
+import RootLayout from "./layouts/rootLayout/RootLayout";
+import DashboardLayout from "./layouts/dashboardLayout/DashboardLayout";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Homepage />,
-  },
-  {
-    path: "/dashboard",
-    element: <DashboardPage />,
-    children: [{path: "/dashboard/chats/:id", element: <ChatPage />}]
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Homepage />,
+      },
+      {
+        element: <DashboardLayout />,
+        children: [
+          { path: "/dashboard", element: <DashboardPage /> },
+          { path: "/dashboard/chats/:id", element: <ChatPage /> },
+        ],
+      },
+    ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
