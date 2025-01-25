de6c3429e0809400c2e6d248b234d9f1ec1afcf8
Itzhak Levy <itzhaklevydev@gmail.com>
2025-01-25 18:10:01 +0200

    Fix homepage text and image

--- src/routes/homepage/Homepage.jsx
+++ src/routes/homepage/Homepage.jsx
@@ -12,10 +12,7 @@ const Homepage = () => {
       <div className="left">
         <h1>LAMA AI</h1>
         <h2>Supercharge your creativity and productivity</h2>
-        <h3>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat sint
-          dolorem doloribus, architecto dolor.
-        </h3>
+        <h3>The future is now 💫</h3>
         <Link to="/dashboard">Get Started</Link>
       </div>
       <div className="right">
