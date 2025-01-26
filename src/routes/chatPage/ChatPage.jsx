148eff00a4662b1351add4d2a7c9487c947f40a3
Itzhak Levy <itzhaklevydev@gmail.com>
2025-01-26 14:59:11 +0200

    Add hard coded prompt page

--- src/routes/chatPage/ChatPage.jsx
+++ src/routes/chatPage/ChatPage.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import "./chatPage.css";
+import NewPropmt from "../../components/newPrompt/NewPrompt";
 
 const ChatPage = () => {
+
   return (
-    <div>
-      <h1>ChatPage</h1>
-      {/* Add dashboard content here */}
+    <div className="chatPage">
+      <div className="wrapper">
+        <div className="chat">
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <NewPropmt />
+        </div>
+      </div>
     </div>
   );
 };
